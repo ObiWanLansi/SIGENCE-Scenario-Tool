f8fa775d0037c6074c37a62b752ac86eb32b3dcd
ObiWanLansi <joerg.lanser@gmail.com>
2018-06-11 09:11:09 +0200

    General Refactoring For Code Quality And Future Development #40

--- SIGENCEScenarioTool.MainApp/Src/Models/EnumerationsAndConstants.cs
+++ SIGENCEScenarioTool.MainApp/Src/Models/EnumerationsAndConstants.cs
@@ -7,10 +7,17 @@ namespace SIGENCEScenarioTool.Models
     /// </summary>
     public enum RxTxType : byte
     {
+        // For all receivers (i.e. ID’s < 0) this parameter defines the radio being used:
         IdealSDR = 0,
         HackRF = 1,
         TwinRx = 2,
 
+        // For transmitters (i.e. ID’s >= 0) this parameter defines transmitter signal type:
+        QPSK = 101,
+        SIN = 102,
+        FMRadio = 103,
+
+        // Should not happen, but you never know ...
         Unknown = 255
 
     } // end public enum RxTxType
@@ -34,11 +41,11 @@ namespace SIGENCEScenarioTool.Models
     public enum DeviceType : byte
     {
         Unknown,
-        
+
         Receiver,
-        
+
         Transmitter,
-        
+
         Reference
 
     } // end public enum DeviceType
