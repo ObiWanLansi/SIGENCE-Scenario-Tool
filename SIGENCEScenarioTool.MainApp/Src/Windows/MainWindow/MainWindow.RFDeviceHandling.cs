465f827cd405878ffc784a81ea9c4733467121f2
ObiWanLansi <joerg.lanser@gmail.com>
2018-07-02 08:09:07 +0200

    Open A Location In Google Maps #56

--- SIGENCEScenarioTool.MainApp/Src/Windows/MainWindow/MainWindow.RFDeviceHandling.cs
+++ SIGENCEScenarioTool.MainApp/Src/Windows/MainWindow/MainWindow.RFDeviceHandling.cs
@@ -717,7 +717,8 @@ namespace SIGENCEScenarioTool.Windows.MainWindow
             }
 
             //string strUrl = string.Format(new NumberFormatInfo { NumberDecimalSeparator = "." }, "https://www.google.de/maps/@{0},{1},500m/sensorset=!3m1!1e3", rfdefvice.Latitude, rfdefvice.Longitude);
-            string strUrl = string.Format(new NumberFormatInfo { NumberDecimalSeparator = "." }, "https://www.google.de/maps/@{0},{1},100m", rfdefvice.Latitude, rfdefvice.Longitude);
+            //string strUrl = string.Format( new NumberFormatInfo { NumberDecimalSeparator = "." } , "https://www.google.de/maps/@{0},{1},100m" , rfdefvice.Latitude , rfdefvice.Longitude );
+            string strUrl = string.Format( new NumberFormatInfo { NumberDecimalSeparator = "." } , "https://www.google.de/maps/place/{0},{1}" , rfdefvice.Latitude , rfdefvice.Longitude );
 
             Tools.Windows.OpenWebAdress(strUrl);
         }
