f925a1c436249290c4dd20156e451c63daea1fa8
ObiWanLansi <joerg.lanser@gmail.com>
2018-10-02 21:04:46 +0200

    General Refactoring For Code Quality And Future Development #40

--- Mockups/SIGENCEScenarioTool.Android/Properties/AssemblyInfo.cs
+++ Mockups/SIGENCEScenarioTool.Android/Properties/AssemblyInfo.cs
@@ -1,20 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("ArcGISApp")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("ArcGISApp")]
-[assembly: AssemblyCopyright ("Copyright ©  2018")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-[assembly: ComVisible (false)]
+[assembly: AssemblyTitle("SIGENCEScenarioTool.Android")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("LansiTools")]
+[assembly: AssemblyProduct("SIGENCEScenarioTool.Android")]
+[assembly: AssemblyCopyright("Copyright © LansiTools 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -26,5 +24,5 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("1.0.0.0")]
-[assembly: AssemblyFileVersion ("1.0.0.0")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
