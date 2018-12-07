b8130f5a6f8f7a9189ebe7adb18518cbc853aefc
ObiWanLansi <joerg.lanser@gmail.com>
2018-12-07 19:45:44 +0100

    Mark To Points In The Map And Create Devices Between Them #168

--- Source/SIGENCEScenarioTool.Library/Src/Tools/GeoHelper.cs
+++ Source/SIGENCEScenarioTool.Library/Src/Tools/GeoHelper.cs
@@ -21,12 +21,12 @@ namespace SIGENCEScenarioTool.Tools
     public static class GeoHelper
     {
         /// <summary>
-        /// 
+        /// The wkbreader
         /// </summary>
         private static readonly WKBReader wkbreader = new WKBReader();
 
         /// <summary>
-        /// 
+        /// The wkbwriter
         /// </summary>
         private static readonly WKBWriter wkbwriter = new WKBWriter();
 
@@ -34,34 +34,59 @@ namespace SIGENCEScenarioTool.Tools
 
 
         /// <summary>
-        /// 
+        /// The germany centerpoint
         /// </summary>
-        public static readonly Point GERMANY_CENTERPOINT = new Point( 51.133333333333 , 10.416666666667 );
+        public static readonly Point GERMANY_CENTERPOINT = new Point( 51.133333333333, 10.416666666667 );
+
+        /// <summary>
+        /// The portanigra
+        /// </summary>
+        public static readonly Point PORTANIGRA = new Point( 49.759652, 6.643998 );
+
+        /// <summary>
+        /// The kaiserthermen
+        /// </summary>
+        public static readonly Point KAISERTHERMEN = new Point( 49.749856, 6.641915 );
+
+        /// <summary>
+        /// The barbarathermen
+        /// </summary>
+        public static readonly Point BARBARATHERMEN = new Point( 49.750309, 6.630350 );
+
+        /// <summary>
+        /// The amphitheater
+        /// </summary>
+        public static readonly Point AMPHITHEATER = new Point( 49.748011, 6.649092 );
+
+        /// <summary>
+        /// The römerbrücke
+        /// </summary>
+        public static readonly Point RÖMERBRÜCKE = new Point( 49.751893, 6.626421 );
 
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
         /// <summary>
-        /// 
+        /// Strings to geometry.
         /// </summary>
-        /// <param name="strWKBAsString"></param>
+        /// <param name="strWKBAsString">The string WKB as string.</param>
         /// <returns></returns>
         public static IGeometry StringToGeometry( string strWKBAsString )
         {
-            if( strWKBAsString == null || strWKBAsString.Trim().Length == 0 )
+            if(strWKBAsString == null || strWKBAsString.Trim().Length == 0)
             {
                 return null;
             }
 
             int iCount = strWKBAsString.Length >> 1;
 
-            byte [] baData = new byte [iCount];
+            byte[] baData = new byte[iCount];
 
-            using( StringReader sr = new StringReader( strWKBAsString ) )
+            using(StringReader sr = new StringReader( strWKBAsString ))
             {
-                for( int i = 0 ; i < iCount ; i++ )
+                for(int i = 0 ; i < iCount ; i++)
                 {
-                    baData [i] = Convert.ToByte( new string( new [] { ( char ) sr.Read() , ( char ) sr.Read() } ) , 16 );
+                    baData[i] = Convert.ToByte( new string( new[] { (char)sr.Read(), (char)sr.Read() } ), 16 );
                 }
             }
 
@@ -70,25 +95,25 @@ namespace SIGENCEScenarioTool.Tools
 
 
         /// <summary>
-        ///
+        /// Geometries to string.
         /// </summary>
-        /// <param name="geo"></param>
+        /// <param name="geo">The geo.</param>
         /// <returns></returns>
         public static string GeometryToString( IGeometry geo )
         {
             // Kann ja passieren, ist ja auch nicht weiter schlimm, liefern wir halt eben auch null zurück.
-            if( geo == null )
+            if(geo == null)
             {
                 return null;
             }
 
-            byte [] baData = wkbwriter.Write( geo );
+            byte[] baData = wkbwriter.Write( geo );
 
             StringBuilder sb = new StringBuilder( baData.Length << 1 );
 
-            foreach( byte b in baData )
+            foreach(byte b in baData)
             {
-                sb.AppendFormat( "{0:X2}" , b );
+                sb.AppendFormat( "{0:X2}", b );
             }
 
             return sb.ToString();
@@ -98,13 +123,14 @@ namespace SIGENCEScenarioTool.Tools
 
 
         /// <summary>
-        ///
+        /// Creates the polygon.
         /// </summary>
-        /// <param name="points"></param>
+        /// <param name="points">The points.</param>
         /// <returns></returns>
-        public static Polygon CreatePolygon( params Point [] points )
+        /// <exception cref="ArgumentException">Es müssen mindestens drei Punkte übergeben werden!</exception>
+        public static Polygon CreatePolygon( params Point[] points )
         {
-            if( points == null || points.Length < 3 )
+            if(points == null || points.Length < 3)
             {
                 throw new ArgumentException( "Es müssen mindestens drei Punkte übergeben werden!" );
             }
@@ -115,7 +141,7 @@ namespace SIGENCEScenarioTool.Tools
             lPoints.AddRange( points.Select( p => p.Coordinate ) );
 
             // Damit es aber auch wirklich ein Polygon und nicht nur eine Line ist müssen wir den ersten Punkten noch hinten drann hängen damit es auch geschlossen wird.
-            lPoints.Add( points [0].Coordinate );
+            lPoints.Add( points[0].Coordinate );
 
             // Aus dem LinearRing können wir das Polygon erzeugen.
             return new Polygon( new LinearRing( lPoints.ToArray() ) );
@@ -123,14 +149,14 @@ namespace SIGENCEScenarioTool.Tools
 
 
         /// <summary>
-        /// 
+        /// Coordinates to point lat LNG.
         /// </summary>
-        /// <param name="c"></param>
+        /// <param name="c">The c.</param>
         /// <returns></returns>
         public static PointLatLng CoordinateToPointLatLng( Coordinate c )
         {
-            return new PointLatLng( c.Y , c.X );
+            return new PointLatLng( c.Y, c.X );
         }
 
-    } // end static public class GeoHelper
+    } // end public static class GeoHelper
 }
---
+++ Source/SIGENCEScenarioTool.UnitTests/Src/UnitTests/DeveloperTests/SIGENCEScenarioToolGeoAlgorithmsTest.cs
@@ -0,0 +1,68 @@
+﻿using log4net;
+
+using Microsoft.SqlServer.Types;
+
+using NetTopologySuite.Geometries;
+
+using NUnit.Framework;
+
+using SIGENCEScenarioTool.Tools;
+using SIGENCEScenarioTool.UnitTest.Attributes;
+
+namespace SIGENCEScenarioTool.UnitTests
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [TestFixture]
+    [SIGENCEScenarioToolTestAction]
+    internal sealed class SIGENCEScenarioToolGeoAlgorithmsTest
+    {
+        /// <summary>
+        /// Logger zum Ausgeben der Protokollierung.
+        /// </summary>
+        private static readonly ILog Log = LogManager.GetLogger( typeof( SIGENCEScenarioToolGeoAlgorithmsTest ) );
+
+        //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// The wg S84
+        /// </summary>
+        private const int WGS84 = 4326;
+
+        /// <summary>
+        /// Gets the geometry.
+        /// </summary>
+        /// <param name="p">The p.</param>
+        /// <returns></returns>
+        private static SqlGeometry GetSqlGeometry( Point p )
+        {
+            return SqlGeometry.Point( p.X, p.Y, WGS84 );
+        }
+
+
+        /// <summary>
+        /// Test000s the distance.
+        /// </summary>
+        [Test, Category( "GeoAlgorithms" ), SIGENCEScenarioToolTestCase( "f66d4f65-bafc-401d-bb2b-7c75fe6efc17" ), Description( "Tests some geo algorithms." )]
+        public void Test000_Distance()
+        {
+            SIGENCEScenarioToolTestCaseHelper.ShowTestCaseInformation();
+
+            //-----------------------------------------------------------------
+
+            var p1 = GetSqlGeometry( GeoHelper.PORTANIGRA );
+            var p2 = GetSqlGeometry( GeoHelper.AMPHITHEATER );
+
+            var dist = p1.STDistance( p2 );
+            Log.InfoFormat( "Port Nigra -> Amphitheater :{0}", dist * 100 );
+
+            double result = GeoHelper.PORTANIGRA.Distance( GeoHelper.AMPHITHEATER );
+            Log.InfoFormat( "Port Nigra -> Amphitheater :{0}", result * 100 );
+
+            var x = NetTopologySuite.Operation.Distance.DistanceOp.Distance( GeoHelper.PORTANIGRA, GeoHelper.AMPHITHEATER );
+            Log.InfoFormat( "Port Nigra -> Amphitheater :{0}", x * 100 );
+        }
+
+    } // end sealed class SIGENCEScenarioToolGeoAlgorithmsTest
+}
